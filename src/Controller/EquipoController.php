c71a442c6e80253be0e2a72bb1faee4d7105db35
FrancescFe <fraferru@gmail.com>
2022-02-15 17:58:03 +0100

    2022-02-15

--- src/Controller/EquipoController.php
+++ src/Controller/EquipoController.php
@@ -4,13 +4,21 @@ namespace App\Controller;
 use App\Entity\Equipos;
 use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class EquipoController extends AbstractController
 {
-    public function listOfEquipos(){
-        $equipos = $this->getDoctrine()->getManager()->getRepository(Equipos::class)
-            ->findListOfEquipos();
+    public function listOfTeams(){
+        $teams = $this->getDoctrine()->getManager()->getRepository(Equipos::class)->findListOfTeams();
 
-        return new JsonResponse($equipos);
+        return new JsonResponse($teams);
+    }
+
+    public function infoOfATeam(Request $request){
+        $aTeam = $request->get('teamName');
+
+        $teams = $this->getDoctrine()->getManager()->getRepository(Equipos::class)->findInfoOfATeam($aTeam);
+
+        return new JsonResponse($teams);
     }
 }
--- src/Repository/EquiposRepository.php
+++ src/Repository/EquiposRepository.php
@@ -1,13 +1,22 @@
 <?php
 
 namespace App\Repository;
+use App\Entity\Equipos;
 use \Doctrine\ORM\EntityRepository;
 
 class EquiposRepository extends EntityRepository
 {
-    public function findListOfEquipos(){
+    public function findListOfTeams(){
         $dql = "SELECT e FROM App:Equipos e";
         $query = $this->getEntityManager()->createQuery($dql);
+
+        return $query->getArrayResult();
+    }
+
+    public function findInfoOfATeam($teamName){
+        $dql = "SELECT e FROM App:Equipos e WHERE e.nombre = :teamName";
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('teamName', $teamName);
 
         return $query->getArrayResult();
     }
